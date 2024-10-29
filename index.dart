114310c6941cbc479ebbf557bf57bb4922b183a2
navruzbek200 <navrozbekbektemirov7@gmail.com>
2024-10-29 10:23:30 +0500

    index.dart commit

--- index.dart
+++ index.dart
@@ -1,5 +1,19 @@
-void main(){
+void main() {
+  String title = "hello lesson #3";
+  title = "welcome to the lesson #3";
 
-print('SALOM DART');
+  int likes = 999;
+  likes = 1000;
 
-}
+  double Ielts = 5.5;
+  Ielts = 6.5;
+
+  bool good = false;
+  good = true;
+
+  print(title);
+  print(likes);
+  print(Ielts);
+  print(good);
+
+} 
